6bb9a980ee028d8de8ff05aed5b17049a19a3c70
Ahmed Hassaballah <ahmedhassaballah727@gmail.com>
2024-12-03 05:16:20 +0200

    Boss Animations

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
