335e9ca5dcfeb2d510340caf6e4db0dc599dbb0e
Ahmed Hassaballah <ahmedhassaballah727@gmail.com>
2024-12-04 20:29:58 +0200

    Boss 2 animations
    spikes + shield

--- Assets/Scripts/BossController.cs
+++ Assets/Scripts/BossController.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class BossController : MonoBehaviour
 {
     Animator anim;
+    public GameObject minion;
     void Start()
     {
         anim = GetComponent<Animator>();
+        minion = GetComponent<GameObject>();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
+
 }
